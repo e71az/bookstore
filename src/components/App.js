fa0922342fd563873487f257b211338bd970b1fe
Elias Castañeda <e71az@icloud.com>
2021-04-21 04:28:40 -0600

    Added the dispatch to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addBook } from '../reducers/books';
 import BooksForm from './BooksForm';
 import BooksList from './BooksList';
 
 const App = () => {
   const { booksArray } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
 
   return (
     <div>
@@ -12,6 +14,9 @@ const App = () => {
         Books:
         {console.log(booksArray)}
       </h1>
+      <button type="button" onClick={() => dispatch(addBook())}>
+        Add Book
+      </button>
       <BooksList />
       <BooksForm />
     </div>

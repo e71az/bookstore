6bdd4c7b4d03cb030a47234e92f8a5e31fdb4fa5
Elias Castañeda <e71az@icloud.com>
2021-04-22 09:46:37 -0600

    Implemented checkIfEmpty for the booksArray in BooksList

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -3,6 +3,8 @@ import Book from '../components/Book';
 
 const BooksList = () => {
   const { booksArray } = useSelector((state) => state.books);
+  // eslint-disable-next-line max-len
+  const checkIfEmtpy = booksArray.length === 0 ? 'No books' : booksArray.map((book) => (<Book key={book.ID} book={book} />));
 
   return (
     <table>
@@ -11,9 +13,7 @@ const BooksList = () => {
         <th>Title</th>
         <th>Category</th>
       </tr>
-      {booksArray.map((book) => (
-        <Book key={book.ID} book={book} />
-      ))}
+      {checkIfEmtpy}
     </table>
   );
 };
